8df0334ace6217372b746bb6716759caa27b7d0d
Fager Wahab De La Fuente <fwahab-d@c2r1s1.42urduliz.com>
2026-07-14 15:48:00 +0200

    Files uploaded

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fwahab-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 21:03:32 by fwahab-d          #+#    #+#             */
-/*   Updated: 2026/07/14 12:44:51 by fwahab-d         ###   ########.fr       */
+/*   Updated: 2026/07/14 15:47:31 by fwahab-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_sort_int_tab(int *tab, int size)
 {
 	int	index;
-	int 	tmp;
+	int	tmp;
 
 	index = size - 1;
 	while (index != 0)
@@ -22,7 +22,8 @@ void	ft_sort_int_tab(int *tab, int size)
 		{
 			tmp = tab[index];
 			tab[index] = tab[index - 1];
-			
+			tab[index - 1] = tmp;
 		}
+		index--;
 	}
 }
